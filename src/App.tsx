0a5a70fbf9e562a7ce3227fe83185702b4661d09
eto-NaN <pierre.pruvost@epitech.eu>
2026-03-22 20:17:15 +0100

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <nav>{/* Navigation */}</nav>
+      </header>
+
+      <main>{/* Sections principales */}</main>
+
+      <footer>{/* Footer */}</footer>
+    </div>
+  );
+}
+
+export default App;
