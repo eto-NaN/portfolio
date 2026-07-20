2dd652d1e90f35ba284f3513aae742320e559e8d
eto-NaN <pierre.pruvost@epitech.eu>
2026-07-20 22:27:56 +0200

    feat(Projects): base setup for projects cards

---
+++ src/components/Projects/data.tsx
@@ -0,0 +1,48 @@
+export interface PorjectCard {
+  id: number,
+  img: string;
+  title: string;
+  description: string;
+  link: string;
+  tags: string[];
+}
+
+export const ProjectsCardTab : PorjectCard[] = [
+
+{
+  id: 1,
+  img: "",
+  title: "",
+  description: "",
+  link: "/",
+  tags: [],
+},
+
+{
+  id: 2,
+  img: "",
+  title: "",
+  description: "",
+  link: "/",
+  tags: [],
+},
+
+{
+  id: 3,
+  img: "",
+  title: "",
+  description: "",
+  link: "/",
+  tags: [],
+},
+
+{
+  id: 4,
+  img: "",
+  title: "",
+  description: "",
+  link: "/",
+  tags: [],
+}
+
+]
