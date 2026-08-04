9825838303518c8e09bd827ad2e3bfe6f99190a8
eto <pierre.pruvost@epitech.eu>
2026-08-05 00:41:08 +0200

    feat(footer): mail and linkedin links via icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,22 @@
 import styles from "./Footer.module.css";
+import mailLogo from "../../assets/images/mail-logo.png";
+import linkedinLogo from "../../assets/images/linkedin-logo.png";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2026 Pierre Pruvost - pierre.pruvost@epitech.eu</p>
+      <p>© 2026 Pierre Pruvost</p>
+      <div className={styles.footerLinks}>
+        <a href="mailto:pierre.pruvost@epitech.eu" target="_blank">
+          <img src={mailLogo} alt="Check my github" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/pierre-pruvost-132121243/"
+          target="_blank"
+        >
+          <img src={linkedinLogo} alt="Check my linkedin" />
+        </a>
+      </div>
     </footer>
   );
 }
