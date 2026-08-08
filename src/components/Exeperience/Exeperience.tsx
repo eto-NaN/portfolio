df362fa791349607a21ed7663bf8b14f6c276b7b
eto <pierre.pruvost@epitech.eu>
2026-08-08 22:09:17 +0000

    feat(experience): base setup for experience section

---
+++ src/components/Exeperience/Exeperience.tsx
@@ -0,0 +1,10 @@
+import styles from "./Experience.module.css";
+
+export default function Experience() {
+  return (
+    <section
+      className={styles.experience}
+      aria-label="Experience section"
+    ></section>
+  );
+}
