743d5a31a9913b215b883fa113fe2c224b6fb036
eto <pierre.pruvost@epitech.eu>
2026-08-13 10:29:32 +0000

    feat(experience): data for Data scientist xp

--- src/components/Exeperience/data.tsx
+++ src/components/Exeperience/data.tsx
@@ -23,4 +23,17 @@ export const ExperiencesCardTab: ExperienceCard[] = [
     link: "",
     tags: ["Teaching", "C", "C++", "Haskell"],
   },
+  {
+    id: 2,
+    img: "",
+    title: "Data scientist",
+    date: "feb 2025 - jul 2026",
+    company: "Caisse des dépôts et consignations",
+    shortDescription:
+      "Building tools with datas to enhance CPF and combat fraud on it",
+    bigDescription:
+      "CPF (https://travail-emploi.gouv.fr/le-compte-personnel-de-formation-cpf) is a product created and managed by the company. As it concerns every worker in France, there are many datas to analyze/use and as it involves a lot of money, there are many fraudsters. During this internship, i built tools allowing fraud prevention officer to better do there job (creation of relationship graphs between individuals and organizations for exemple). I also contribuated to create a new search engine (benchmarking and comparative evaluation of natural language processing (NLP) models algorithm). As I worked with data scientists, I had the opportunity to bring them some good practice in their works such as a better use of git alone and in team.",
+    link: "",
+    tags: ["NLP", "Data", "IHM"],
+  },
 ];
